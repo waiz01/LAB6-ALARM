23beae7997b81e9999bc1496f9836cde417174fb
waiz01 <2020628062@student.uitm.edu.my>
2021-12-11 12:48:24 +0800

    LAB 6 ALARM UWU

---
+++ app/src/main/java/com/example/myapplication/AlarmReceiver.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import static androidx.legacy.content.WakefulBroadcastReceiver.startWakefulService;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Notification received!", Toast.LENGTH_LONG).show();
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(2000);
+
+        //Stop sound service to play sound for alarm
+        context.startService(new Intent(context, AlarmSoundService.class));
+
+        //This will send a notification message and show notification in notification tray
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                AlarmNotificationService.class.getName());
+        startWakefulService(context, (intent.setComponent(comp)));
+    }
+}
